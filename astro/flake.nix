e5ed4a8fd7bb4dd468ff21bbe6891de1f111b8e2
jamibear <5701ja.mi@gmail.com>
2024-03-25 10:34:46 +0800

    write derivation for astro flake

--- astro/flake.nix
+++ astro/flake.nix
@@ -10,11 +10,26 @@
     flake-utils.lib.eachDefaultSystem ( system:
         let 
             pkgs = import nixpkgs { inherit system; };
+
+            astro = pkgs.writeShellApplication {
+                name = "astro";
+                runtimeInputs = [ pkgs.nodejs_21 ];
+                text = ''
+                   # serve localhost to port 4321
+                   npm install 
+                   npm run dev 
+                '';
+            };
         in with pkgs; {
             devShells.default = mkShell {
                 buildInputs = [
                     nodejs_21
                 ];
+            };
+
+            apps.default = {
+                type = "app";
+                program = "${astro}/bin/astro";
             };
         }
     );
